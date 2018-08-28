73246a983c37048e3fee4a1522fe99b3ce85a55f
李文岭 <lwl_0321@126.com>
2018-08-28 11:45:30 +0800

    aa

---
+++ frontend/config/main-local.php
@@ -0,0 +1,24 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'DCXynIkJBd2uc90p6-0F7KChpbMA0Z_2',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
